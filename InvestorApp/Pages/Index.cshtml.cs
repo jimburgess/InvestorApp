0fb8c63c884698ff2b2ee90597116ea697a19170
James <38355089+jb9280@users.noreply.github.com>
2021-05-23 19:41:30 +0100

    Add front end controls to app

--- InvestorApp/Pages/Index.cshtml.cs
+++ InvestorApp/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,8 +12,32 @@ namespace InvestorApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        [Display(Name = "Lump Sum Investment (£)")]
+        [Required]
+        public decimal LumpSum { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [Display(Name = "Monthly Investment (£)")]
+        [Required]
+        public decimal Monthly { get; set; }
+
+        [Display(Name = "Target Value (£)")]
+        [Required]
+        public decimal Target { get; set; }
+
+        [Display(Name = "Timescale (years)")]
+        [Required]
+        public int Timescale { get; set; }
+
+        [Display(Name = "Risk Level")]
+        [Required]
+        public Level RiskLevel { get; set; }
+
+        public enum Level
+        {
+            Low, Medium, High
+        }
+    
+    public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
