6d90f4e120418cdf78b057fea60bfea03312804d
James <38355089+jb9280@users.noreply.github.com>
2021-05-25 00:49:10 +0100

    Add graph

---
+++ InvestorApp/Models/GraphData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvestorApp.Models
+{
+    public class GraphData
+    {
+        public string[] Labels { get; set; }
+        public IEnumerable<Dataset> Datasets { get; set; }
+
+        public class Dataset
+        {
+            public string Label { get; set; }
+            public string BackgroundColour { get; set; }
+            public string BorderColour { get; set; }
+            public IEnumerable<decimal> Data { get; set; }
+        }
+    }
+}
--- InvestorApp/Pages/Index.cshtml.cs
+++ InvestorApp/Pages/Index.cshtml.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -42,7 +44,12 @@ namespace InvestorApp.Pages
         {
             Low, Medium, High
         }
-
+        //[BindProperty (SupportsGet =true)]
+        //public string [] XLabels { get; set; }
+        //[BindProperty(SupportsGet = true)]
+        //public IEnumerable<InvestmentData.Bound> Bounds { get; set; }
+        [BindProperty (SupportsGet =true)]
+        public string GraphData { get; set; }
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -50,18 +57,48 @@ namespace InvestorApp.Pages
 
         public void OnGet()
         {
-
+            
         }
-        public async Task OnPostAsync()
+        public async Task<IActionResult> OnPostAsync()
         {
             var request = new Request {LumpSum = LumpSum, Monthly=Monthly,RiskLevel=RiskLevel.ToString(),Target=Target, Timescale= Timescale
             };
-            var json = JsonSerializer.Serialize(request);
-            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var jsonRequest = JsonSerializer.Serialize(request);
+            var dataRequest = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
             using (var httpClient = new HttpClient())
             {
-                var result = await httpClient.PostAsync("http://localhost:5000/api/Investor/Post",data);
+                var result = await httpClient.PostAsync("http://localhost:5000/api/Investor/Post",dataRequest);
+                if (result.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await result.Content.ReadAsStringAsync();
+                    if (!string.IsNullOrWhiteSpace(jsonResponse))
+                    {
+                        InvestmentData data = JsonSerializer.Deserialize<InvestmentData>(jsonResponse,new JsonSerializerOptions {PropertyNameCaseInsensitive=true });
+                        if (data != null)
+                        {
+                            //XLabels = data.Risks.FirstOrDefault().Bounds.FirstOrDefault().Months.Select(x => x.Index.ToString()).ToArray<string>();
+                            var bounds = data.Risks.FirstOrDefault().Bounds.ToList();
+                            var graphData = new GraphData();
+                            graphData.Labels = bounds.FirstOrDefault().Months.Select(x => x.Index.ToString()).ToArray<string>();
+                            var datasets = new List<GraphData.Dataset>();
+                            foreach (var bound in bounds)
+                            {
+                                datasets.Add(
+                                    new GraphData.Dataset { 
+                                        BackgroundColour = "rgb(255, 99, 132)", 
+                                        BorderColour = "rgb(255, 99, 132)", 
+                                        Data = bound.Months.Select(x => x.Balance), Label = bound.BoundType.ToString() });
+                            }
+                            graphData.Datasets = datasets;
+                            GraphData = JsonSerializer.Serialize(graphData, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+                                
+                        }
+                        
+                    }
+                }
+                
             }
+            return Page();
         }
     }
 }
